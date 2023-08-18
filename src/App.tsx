9651e21f8fa04084a1601f81e8ef29b941b6d35d
George Griffiths <george.griffiths@immersivelabs.com>
2023-08-18 20:53:29 +0100

    added subpath

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { Github, GithubIcon } from "lucide-react";
 import "./App.css";
 import { KanbanBoard } from "./components/KanbanBoard";
 import { ThemeToggle } from "./components/ThemeToggle";
 import { ThemeProvider } from "./components/theme-provider";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
         <div className="min-h-screen flex flex-col">
-          <header className="flex items-end w-full flex-col p-4">
+          <header className="flex justify-between w-full flex-row p-4">
+            <Button variant="link" asChild className="text-primary h-16 w-16">
+              <a href="https://github.com/Georgegriff/react-dnd-kit-tailwind-shadcn-ui">
+                <Github className="fill-current h-full w-full" />
+              </a>
+            </Button>
             <ThemeToggle />
           </header>
           <main className="basis-[100%] flex-1 flex items-center">
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+  base: mode === "production" ? "/react-dnd-kit-tailwind-shadcn-ui/" : "/",
+}));
