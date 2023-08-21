a2825d0f8296e789396931e297f98bd455062694
George Griffiths <george.griffiths@immersivelabs.com>
2023-08-21 23:26:10 +0100

    moved additional css

--- src/components/BoardColumn.tsx
+++ src/components/BoardColumn.tsx
@@ -55,7 +55,7 @@ export function BoardColumn({ column, tasks, isOverlay }: BoardColumnProps) {
   };
 
   const variants = cva(
-    "h-[500px] max-h-[500px] w-[100vw] w-[350px] max-w-full bg-secondary flex flex-col flex-shrink-0 snap-center",
+    "h-[500px] max-h-[500px] w-[350px] max-w-full bg-secondary flex flex-col flex-shrink-0 snap-center",
     {
       variants: {
         dragging: {
